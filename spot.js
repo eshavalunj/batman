915191cc855f6783b046a6d6c5bd959428a8529a
eshavalunj <130785272+eshavalunj@users.noreply.github.com>
2024-06-28 23:44:37 +0530

    Add files via upload

---
+++ spot.js
@@ -0,0 +1,7 @@
+const images = document.querySelectorAll('.image-container img');
+images.forEach(image => image.addEventListener('click', handleImageClick));
+
+function handleImageClick(event) {
+    // Logic to check for differences goes here
+    alert('there is no difference 😜 TU AHES CUPCAKE MAJHA!');
+}
